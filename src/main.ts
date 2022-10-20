19d98ff1078dddbaa54253345ac824b0098347fc
reach0908 <wlsghkim0908@gmail.com>
2022-10-20 23:57:08 +0900

    feat(prettier) : 프레티어 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app: NestApplication = await NestFactory.create(AppModule);
+	const logger = new Logger();
+	await app.listen(3000);
+	logger.log('app start');
 }
 bootstrap();
